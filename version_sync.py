ba601baf1e14b3350ec5f6b1e03be77b2ef67e85
Benjamin Leutner <1650314+bleutner@users.noreply.github.com>
2024-05-16 12:41:27 +0200

    init

--- version_sync.py
+++ version_sync.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
-
 import re
 import sys
 from pathlib import Path
+def main(file=sys.argv[1:]):
+    init_file = Path(file[0])
 
-def main(argv=None):
-    # Check if the commit contains changes to __init__.py
-    print(argv)
-    if "__init__.py" in argv[0]:
-        init_file = Path("__init__.py")
+    with init_file.open() as f:
+        version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
-        with init_file.open() as f:
-            version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
-
-        with open("VERSION.txt", "rw") as f:
+    VERSION = (init_file.parents[2]/"VERSION.txt")
+    if not VERSION.exists():
+        print(f"No matching VERSION.txt file found for {init_file}")
+    else:
+        with open(VERSION, "r") as f:
             g = f.read()
+            
             if g != version:
-                f.seek(0)
-                f.write(version)
+                print(f"Updating {VERSION} with version {version}")
+                with open(VERSION, "w") as f:
+                    f.seek(0)
+                    f.write(version)
+            else:
+                print(f"{VERSION} already up to date at {version}")
 
 if __name__ == "__main__":
     main()
